2fb1ea4cba279ea0e9c201f3797ea2efcd6694bf
raminabar <67957413+raminabar@users.noreply.github.com>
2021-03-13 19:16:37 -0500

    Initial commit

---
+++ utilities/database_utilities.py
@@ -0,0 +1,8 @@
+def create_database(database_path: str):
+    #TODO: generate the databse
+    pass
+
+def save_words_to_datbase(database_path: str, words_list: list):
+    #TODO: save the words to the databse
+    pass
+
